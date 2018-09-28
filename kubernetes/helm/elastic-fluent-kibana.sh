54d7ef0d2ea606c6852453cd03918a1a3f3fd94f
Viktor Georgiev <v.georgiev90@abv.bg>
2018-09-28 14:43:38 +0300

    Update elastic-fluent-kibana.sh

--- kubernetes/helm/elastic-fluent-kibana.sh
+++ kubernetes/helm/elastic-fluent-kibana.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-## Install elk stack with helm
+## Add the repo
+helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
+helm repo update
+### Install elastic search
 
-helm repo add akomljen-charts https://raw.githubusercontent.com/komljen/helm-charts/master/charts/
+### Make sure that you specify the storage class in values.yml for elasticsearch-data pod
+## helm fetch incubator/elasticsearch
+## data:
+##  persistence:
+##    enabled: true
+##    accessMode: ReadWriteOnce
+##    name: data
+##    size: "30Gi"
+##    storageClass: "glusterfs"
+## helm install incubator/elasticsearch --namespace logging --name elasticsearch --set data.terminationGracePeriodSeconds=0 -f values.yml
+helm install incubator/elasticsearch --namespace logging --name elasticsearch --set data.terminationGracePeriodSeconds=0 -f values.yml
 
-# es-operator deploy
-helm install --name es-operator --namespace logging --set rbac.create=true akomljen-charts/elasticsearch-operator
+## fluentd install
+helm install --name fluentd --namespace logging stable/fluentd-elasticsearch --set elasticsearch.host=elasticsearch-client.logging.svc.cluster.local,elasticsearch.port=9200
 
-# deploy the stack
-helm install --name efk --namespace logging akomljen-charts/efk
+## kibana install
+helm install --name kibana --namespace logging stable/kibana --set env.ELASTICSEARCH_URL=http://elasticsearch-client.logging.svc.cluster.local:9200,env.SERVER_BASEPATH=/api/v1/namespaces/logging/services/kibana/proxy
 
-#Create ingress for the kibana service on port 5601
+## Reference  https://platform9.com/blog/kubernetes-logging-and-monitoring-the-elasticsearch-fluentd-and-kibana-efk-stack-part-2-elasticsearch-configuration/
 
-##reference 
-# https://akomljen.com/get-kubernetes-logs-with-efk-stack-in-5-minutes/
