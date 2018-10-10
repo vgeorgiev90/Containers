e09ba21b1a86762f027dded02dac2899700202d3
Viktor Georgiev <v.georgiev90@abv.bg>
2018-10-10 14:13:45 +0300

    Update pre-warm.sh

--- kubernetes/deploy-with-jenkins/pre-warm.sh
+++ kubernetes/deploy-with-jenkins/pre-warm.sh
@@ -13,4 +13,5 @@ echo "Executing on pod: ${pod}"
 sleep 120
 kubectl exec ${pod} /mnt/deploy-and-start.sh ${db_name} ${db_user} ${db_pass} ${db_host} ${domain} -n deploy
 sleep 60
+for i in `kubectl get pods -n deploy | grep ${deploy_web} | awk '{print $1}'`;do kubectl exec $i /mnt/start.sh -n deploy;done
 
